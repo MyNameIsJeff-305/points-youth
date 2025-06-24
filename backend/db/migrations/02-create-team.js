dac695f5a1bea680ace7bb7112340b71ab8af537
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-06-23 22:57:01 -0400

    Adding pg npm package to backend folder

--- backend/db/migrations/02-create-team.js
+++ backend/db/migrations/02-create-team.js
@@ -21,7 +21,7 @@ module.exports = {
         allowNull: false
       },
       description: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(500),
         allowNull: false
       },
       logoUrl: {
--- backend/db/models/team.js
+++ backend/db/models/team.js
@@ -4,13 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Team extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
   }
   Team.init({
@@ -20,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
       unique: true
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(500),
       allowNull: false
     },
     logoUrl: {
